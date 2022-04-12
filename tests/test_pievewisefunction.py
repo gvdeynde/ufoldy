aef844b545d1c91730deddeb0804b8cfa8ffcb6f
Van den Eynde Gert <gert.van.den.eynde@sckcen.be>
2022-04-12 11:05:03 +0200

    made piecewisefuncton abstract base class. implemented the linear piecewise function.

---
+++ tests/test_pievewisefunction.py
@@ -0,0 +1,302 @@
+# -*- coding: utf-8 -*-
+
+""" Tests for the `piecewiselinear` module."""
+
+import pytest
+from pytest import approx
+import numpy as np
+from scipy.integrate import quad
+from ufoldy.plf import PLF
+
+
+def argsort(seq):
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
+
+@pytest.fixture
+def small():
+    x = [0, 1, 3]
+    y = [1, 2, -1]
+
+    norm = 2.5
+    integral13 = 1.0
+    integral15 = 1.0
+
+    return x, y, norm, integral13, integral15
+
+
+def test_init_from_sorted_list(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    for i in range(len(x)):
+        assert x[i] == p.x[i]
+        assert y[i] == p.y[i]
+
+
+def test_init_from_unsorted_list(small):
+    x, y, *r = small
+
+    x = x[::-1]
+    y = y[::-1]
+
+    p = PLF(x, y)
+
+    xx = x.copy()
+    yy = y.copy()
+
+    idx = argsort(xx)
+
+    for i in range(len(x)):
+        assert xx[idx[i]] == p.x[i]
+        assert yy[idx[i]] == p.y[i]
+
+
+def test_init_wrong_xlist():
+    x = []
+    y = [1, 2, 3]
+
+    with pytest.raises(ValueError):
+        p = PLF(x, y)
+
+
+def test_init_wrong_ylist():
+    x = [1, 2, 3]
+    y = []
+
+    with pytest.raises(ValueError):
+        p = PLF(x, y)
+
+
+def test_init_noncompatlist():
+    x = [1, 2]
+    y = [1, 2, 3]
+
+    with pytest.raises(ValueError):
+        p = PLF(x, y)
+
+
+def test_init_nparray(small):
+    x, y, *r = small
+
+    xa = np.array(x)
+    ya = np.array(y)
+
+    p = PLF(xa, ya)
+
+    for i in range(len(x)):
+        assert xa[i] == p.x[i]
+        assert ya[i] == p.y[i]
+
+
+def test_init_x2darray():
+    x = np.array([[1, 2], [3, 4]])
+    y = np.array([1, 2, 3, 4])
+
+    with pytest.raises(ValueError):
+        p = PLF(x, y)
+
+
+def test_init_y2darray():
+    x = np.array([1, 2, 3, 4])
+    y = np.array([[1, 2], [3, 4]])
+
+    with pytest.raises(ValueError):
+        p = PLF(x, y)
+
+
+def test_evaluate(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    assert p(-1) == approx(0.0)
+    assert p(0) == approx(1.0)
+    assert p(0.5) == approx(1.5)
+    assert p(1) == approx(2)
+    assert p(2) == approx(0.5)
+    assert p(7 / 3) == approx(0.0)
+    assert p(3) == approx(-1.0)
+    assert p(4) == approx(0.0)
+
+    assert p([-1, 0, 0.5, 1, 2, 7 / 3, 3, 4]) == approx(
+        np.array([0.0, 1.0, 1.5, 2.0, 0.5, 0.0, -1.0, 0.0])
+    )
+
+
+def test_init_normalized(small):
+    x, y, *r = small
+
+    p = PLF(x, y, normalize=True)
+
+    assert p.norm == approx(1.0)
+
+
+def test_init_normalized_to_number(small):
+    x, y, *r = small
+
+    p = PLF(x, y, normalize=True, normvalue=8.0)
+
+    assert p.norm == approx(8.0)
+
+
+def test_init_emptyxy():
+    p = PLF([], [])
+
+    assert p.x[0] == approx(0.0)
+    assert p.x[1] == approx(1.0)
+    assert p.y[0] == approx(0.0)
+    assert p.y[1] == approx(0.0)
+
+
+def test_xsetter(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    with pytest.raises(ValueError):
+        p.x = [-1]
+
+
+def test_ysetter(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    p.y = [1, 1, 3]
+
+    assert p.y[0] == approx(1.0)
+
+    assert p(0.5) == approx(1.0)
+
+
+def test_ysetter_wrongshape(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    newy = [1, 2]
+
+    with pytest.raises(ValueError):
+        p.y = newy
+
+
+def test_slopes_setter(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    with pytest.raises(ValueError):
+        p.slopes = 1.0
+
+
+def test_str(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    strval = "[+0.0000e+00, +1.0000e+00, +3.0000e+00]\n[+1.0000e+00, +2.0000e+00, -1.0000e+00]"
+
+    assert f"{p}" == strval
+
+
+def test_norm(small):
+    x, y, norm, *r = small
+
+    p = PLF(x, y)
+
+    assert norm == approx(p.norm)
+
+
+def test_norm_setter(small):
+    x, y, norm, *r = small
+
+    p = PLF(x, y)
+
+    with pytest.raises(ValueError):
+        p.norm = 1.0
+
+
+def test_insert_point(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    p.insert_points(0.5, 1)
+
+    assert p(0.25) == approx(1.0)
+    assert p(0.5) == approx(1.0)
+    assert p(1.0) == approx(2.0)
+
+
+def test_insert_point_noy(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+
+    p.insert_points(7 / 3)
+
+    assert p(-1) == approx(0.0)
+    assert p(0) == approx(1.0)
+    assert p(0.5) == approx(1.5)
+    assert p(1) == approx(2)
+    assert p(2) == approx(0.5)
+    assert p(7 / 3) == approx(0.0)
+    assert p(3) == approx(-1.0)
+    assert p(4) == approx(0.0)
+
+
+def test_insert_points(small):
+    x, y, *r = small
+
+    p = PLF(x, y)
+    q = PLF(x, y)
+
+    newx = [(p.x[i] + p.x[i + 1]) / 2 for i in range(2)]
+    newy = p(newx)
+
+    q.insert_points(newx, newy)
+
+    evalx = np.linspace(x[0], x[-1], 11)
+
+    for xx in evalx:
+        assert p(xx) == approx(q(xx))
+
+
+def test_copy(small):
+    x, y, *r = small
+
+    a = PLF(x, y)
+
+    b = a.copy()
+
+    assert np.any(a.x == b.x) and not a.x is b.x
+    assert np.any(a.y == b.y) and not a.y is b.y
+
+
+def test_flat_noy():
+    a = PLF.flat(-1, 1)
+
+    assert a.x[0] == approx(-1.0)
+    assert a.x[1] == approx(+1.0)
+    assert a.y[0] == approx(0.5)
+    assert a.y[1] == approx(0.5)
+
+
+def test_flat_y():
+    a = PLF.flat(-1, 1, 10)
+
+    assert a.x[0] == approx(-1.0)
+    assert a.x[1] == approx(+1.0)
+    assert a.y[0] == approx(10.0)
+    assert a.y[1] == approx(10.0)
+
+
+def test_flat_swapx():
+    a = PLF.flat(+1, -1)
+
+    assert a.x[0] == approx(-1.0)
+    assert a.x[1] == approx(+1.0)
+    assert a.y[0] == approx(0.5)
+    assert a.y[1] == approx(0.5)
---
+++ ufoldy/piecewisefunction.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides a class to work with piecewiselinear functions
+"""
+
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class PiecewiseFunction(ABC):
+
+    """Class for piecewise functions, defined by x positions and corresponding y values.
+    It's an abstract base class which needs specialisation for piecewise
+    constant and piecewise linear functions.
+    """
+
+    def __init__(self, x, y, normalize=False, normvalue=1.0):
+        """
+        Args:
+            x (:obj: `list` of :obj:`float` or :obj:`np.ndarray`): the nodes
+            y (:obj: `list` of :obj:`float` or :obj:`np.ndarray`): the values
+            at the nodes
+            normalize (bool): if `True`, the y values are normalized such that
+            :math:`\int f(x) dx = C` where `C` is the normalisation value. Default value is `False`.
+            normvalue (float): the normalisation value `C` to be used.
+        """
+
+        self._x = np.copy(np.asarray(x, dtype=float))
+        self._y = np.copy(np.asarray(y, dtype=float))
+
+        shapex = self._x.shape
+        shapey = self._y.shape
+
+        if len(shapex) != 1:
+            raise ValueError("x needs to be a one-dimensional array")
+
+        if len(shapey) != 1:
+            raise ValueError("y needs to be a one-dimensional array")
+
+        if shapex[0] != shapey[0]:
+            raise ValueError("x and y should have the same length")
+
+        if shapex[0] == 0:
+            # Default PLF using [0, 1] and [0, 0]
+            self._x = np.array([0, 1])
+            self._y = np.array([0, 0])
+            normalize = False
+
+        sortindices = np.argsort(self._x)
+        self._x = self._x[sortindices]
+        self._y = self._y[sortindices]
+
+        if normalize:
+            self._y /= self.norm
+            self._y *= normvalue
+
+    @classmethod
+    def flat(cls, xmin, xmax, y=None):
+        """classmethod to create a PLF that is a flat line
+        between xmin and xmax
+
+        Args:
+            xmin: float left boundary
+            xmax: float right boundary
+            y: float height (if None, the height is set to 1.0/(xmax - xmin))
+
+        Returns:
+            instance of PiecewiseFunction
+        """
+
+        if xmax < xmin:
+            xmin, xmax = xmax, xmin
+
+        if not y:
+            y = 1.0 / (xmax - xmin)
+
+        return cls([xmin, xmax], [y, y])
+
+    @abstractmethod
+    def __call__(self, x):
+        pass
+
+    # @abstractmethod
+    # def _calculate_slopes(self):
+    # pass
+
+    @property
+    @abstractmethod
+    def slopes(self):
+        pass
+        # return self._calculate_slopes()
+
+    @slopes.setter
+    def slopes(self, val):
+        raise ValueError("Slopes is a read-only property")
+
+    @property
+    @abstractmethod
+    def norm(self):
+        pass
+        # return np.trapz(self._y, self._x)
+
+    @norm.setter
+    def norm(self, val):
+        raise ValueError("Norm is a read-only property")
+
+    @property
+    def x(self):
+        """Returns the nodes"""
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        """No self-manipulation of the nodes. Makes things complicated.
+        Use the insert_points function to add nodes. Deleting nodes is not
+        permitted."""
+        raise ValueError("Shouldn't set 'x'")
+
+    @property
+    def y(self):
+        """Returns the function values at the nodes."""
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        """Sets new function values."""
+        self._y = np.asarray(y)
+        if self._x.shape != self._y.shape:
+            raise ValueError("y has wrong shape")
+        else:
+            self._y = y
+
+    def __str__(self):
+        """String representation for a piecewiselinear function."""
+        retval = ""
+
+        retval += np.array2string(
+            self._x,
+            separator=", ",
+            sign="+",
+            formatter={"float_kind": lambda x: "%+.4e" % x},
+        )
+        retval += "\n"
+        retval += np.array2string(
+            self._y,
+            separator=", ",
+            sign="+",
+            formatter={"float_kind": lambda x: "%+.4e" % x},
+        )
+        return retval
+
+    def insert_points(self, newx, newy=None):
+        """Insert new nodes and corresponding function values. Important: the
+        function does not re-normalize the piecewiselinear function!
+
+        Args:
+            newx (:obj: `list` of :obj:`float` or :obj:`np.ndarray`): the nodes
+            to be added
+            newy (:obj: `list` of :obj:`float` or :obj:`np.ndarray`): the
+            corresponding function values.
+
+        Returns:
+            (:obj: `PiecewiseFunction`): self
+        """
+
+        # If new y values not given, calculate them with linear interpolation
+        if newy is None:
+            newy = self(newx)
+
+        indices = np.searchsorted(self._x, newx)
+
+        self._x = np.insert(self._x, indices, newx)
+        self._y = np.insert(self._y, indices, newy)
+
+        return self
+
+    @abstractmethod
+    def copy(self):
+        pass
+
+
+class PLF(PiecewiseFunction):
+    """Class that implements Piecewise Linear Functions. Derived from
+    PiecewiseFunction.
+    """
+
+    # def _calculate_slopes(self):
+    # return np.diff(self._y) / np.diff(self._x)
+
+    def __call__(self, x):
+        # Initialise
+
+        result = np.zeros_like(np.atleast_1d(x))
+
+        # Left end-point
+        result[x == self._x[0]] = self._y[0]
+
+        # Right end-point
+        result[x == self._x[-1]] = self._y[-1]
+
+        # Interpolate
+        idx = np.searchsorted(self._x, x)
+
+        # this is a fixup to avoid problems with left/right endpoint. It just
+        # replaces a too high index with arbitrary 0. idx for that position is
+        # never needed in end result (where in next line) but is needed to
+        # avoid an index error
+        idx = np.where(np.logical_or(idx < 1, idx >= len(self._x)), 1, idx)
+
+        result = np.where(
+            np.logical_and(x > (self._x[0]), (x < self._x[-1])),
+            self._y[idx - 1] + self.slopes[idx - 1] * (x - self._x[idx - 1]),
+            result,
+        )
+
+        return result
+
+    @PiecewiseFunction.slopes.getter
+    def slopes(self):
+        return np.diff(self._y) / np.diff(self._x)
+
+    @PiecewiseFunction.norm.getter
+    def norm(self):
+        return np.trapz(self._y, self._x)
+
+    def copy(self):
+        """
+        Return a full, deep copy of PLF.
+
+        Args:
+            none
+
+        Returns:
+            (:obj: `PLF`): a deep copy of itself
+        """
+        return PLF(self._x, self._y, normalize=True, normvalue=self.norm)
